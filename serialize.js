4fe1f65378bd79e9204a180c309ee8a8dad33518
Tyler Distad <tyler.distad@gmail.com>
2019-12-03 20:18:04 -0500

    serialize accepts promise factories, not promises
    See "Advanced mistake #3" at https://pouchdb.com/2015/05/18/we-have-a-problem-with-promises.html

--- serialize.js
+++ serialize.js
@@ -1,7 +1,7 @@
 // based on https://stackoverflow.com/a/41115086/421313
 const serialize = (listOfFunctions) => { // eslint-disable-line arrow-body-style
   return listOfFunctions.reduce((promise, func) => { // eslint-disable-line arrow-body-style
-    return promise.then(result => func.then(Array.prototype.concat.bind(result)))
+    return promise.then(result => func().then(Array.prototype.concat.bind(result)))
   }, Promise.resolve([]))
 }
 
